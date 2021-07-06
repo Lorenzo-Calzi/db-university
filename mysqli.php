916777a4ed1a0cceba8ed3b579c184a93b379e72
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-07-06 17:28:13 +0200

    Aggiunto foglio di stile

--- mysqli.php
+++ mysqli.php
@@ -17,7 +17,9 @@ $connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 if ($connection && $connection->connect_error) {
     echo 'Sorry, connection failed! ' . $connection->connect_error;
 } else {
-    echo 'Connection successful! Go';
+    ?>
+    <h4 class="message"> <?php echo 'Connection successful! Go 👍';?> </h4>
+    <?php
 }
 
 
@@ -33,9 +35,9 @@ $degree_id = 10;
 $fiscal_code = "CLZLNZ99P23A940H";
 $enrolment_date = "2018-09-15";
 $registration_number = 3451559558;
-$email = "lorenzocalzi@gmail.com";
+$email = "lorenzo@gmail.com";
 $statement->execute();
-var_dump($statement); 
+/* var_dump($statement);  */
 
 
 # 5.
@@ -46,24 +48,29 @@ $results = $connection->query($sql);
 if($results && $results->num_rows > 0) {
     while($row = $results->fetch_assoc()) {
     ?>   
-        <div>
-            <div style="display: flex; align-items: center;">
-                <h4> <?php echo "Studente: "?> <h3> <?php echo $row['name'] . " " . $row['surname'];?> </h3> </h4>
-            </div>
+        <div class="container">
+            <div class="user">
+                <div>
+                    <h3> <?php echo " " . $row['name'] . " " . $row['surname'];?> </h3>
+                </div>
 
-            <p> <?php echo "Nato il: " . $row['date_of_birth'];?> </p>
-            <p> <?php echo "Iscritto il: " . $row['enrolment_date']; ?> </p>
-        </div> 
+                <p> <?php echo "Nato il: " . $row['date_of_birth'];?> </p>
+                <p> <?php echo "Codice fiscale: " . $row['fiscal_code'];?> </p>
+                <p> <?php echo "Iscritto il: " . $row['enrolment_date']; ?> </p>
+                <p> <?php echo "Email: " . $row['email']; ?> </p>
+            </div> 
+        </div>
+        
         
     <?php
     }
 } elseif($results) {
     ?>
-    <h2> <?php echo "0 Results"; ?> </h2>
+    <h2> <?php echo "No Results 😅"; ?> </h2>
     <?php
 } else {
     ?>
-    <h2> <?php  echo "Query error"; ?> </h2>
+    <h2> <?php  echo "Query error 🤷‍♂️"; ?> </h2>
     <?php
 }
 
@@ -72,3 +79,25 @@ if($results && $results->num_rows > 0) {
 $connection->close();
 
 ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500&display=swap" rel="stylesheet">
+    <!-- Link CSS -->
+    <link rel="stylesheet" href="./style.css">
+    <!-- Title -->
+    <title>University</title>
+</head>
+<body>
+    
+</body>
+</html>
